193ec2c88378352e8ccb3f6b6dd17b4b3263fa52
shalini <shalu.garg22@gmail.com>
2022-02-15 14:10:06 +0530

    ecma6 code snnipet commit

---
+++ js/exercise/e01.js
@@ -0,0 +1,46 @@
+// imn
+import './style.css';
+
+//imd
+import {useState} from 'react';
+
+//mde
+module.exports = {
+    
+};
+
+//sti
+setInterval(() => {
+    //do something here
+}, 1000);
+
+//sto
+setTimeout(() => {
+    // do something cool here
+}, 5000);
+
+//nfn 
+const myFunction = (params) => {
+
+}
+
+//dar 
+const [name,setName]= useState('shalini')
+
+//clg 
+console.log('something else');
+
+console.log ('object:',object);
+
+//cwa
+console.warn(object);
+
+
+//cer
+console.error(object);
+
+const name = 'shalini';
+console.log ("TCL: name", name)
+
+const twitter = 'shaliniagrawal';
+console.log ("TCL:twitter",twitter)
