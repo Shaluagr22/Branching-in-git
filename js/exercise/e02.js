3f5f3a125f587a08494e4e1842044ef3707a2b45
shalini <shalu.garg22@gmail.com>
2022-02-15 13:21:59 +0530

    e02 commit

---
+++ js/exercise/e02.js
@@ -0,0 +1,14 @@
+console.time('Your code took time');
+console.log("hello world");//string
+console.log(5+56);//number
+console.log(true);//boolean
+console.log([34,6,73,87]);//array
+console.log({Name:'Shalini' , Age:32}); //object
+console.table({Name:'Shalini' , Age:32});
+console.warn('this is warning');
+// console.clear();
+console.timeEnd('Your code took time');
+// console.assert(34>189,'age>189 is not possible');
+console.error('this is an error');
+// single line comment --> ctrl / (this line will be not execute)
+// multiline comment --> /* this is multiline comment */
