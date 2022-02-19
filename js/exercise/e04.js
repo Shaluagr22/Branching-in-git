4e885257ef566a58c3a2f2f04134aa8fb75cd6ea
shalini <shalu.garg22@gmail.com>
2022-02-19 22:08:13 +0530

    e04 commit

---
+++ js/exercise/e04.js
@@ -0,0 +1,24 @@
+// primitive data types
+
+//string
+let firstName = "Shalini"
+console.log(firstName);
+console.log(typeof firstName)
+console.log(" my string is " +  firstName)
+
+// Number
+let age = 32;
+console.log(age)
+console.log(" Data type is " + (typeof age))
+
+//Boolean
+let isDriving = true;
+console.log(" Data type is " + (typeof isDriving))
+
+//null
+let nullVar = null;
+console.log(" Data type is " + (typeof nullVar)) //object
+
+//Undefined
+let undef = undefined;
+console.log(" Data type is " + (typeof undef))
---
+++ js/exercise/e05.js
@@ -0,0 +1,24 @@
+// Reference Data types
+
+// Arrays
+
+ let myarr = [1, 2, 3, 4, false, "string"];
+console.log(" Data type is " + (typeof myarr));
+
+// Objects Literals
+let marks = {
+    "Shalini": 75 ,
+     "Honey": 95,
+ "Prayaan" : 87
+}
+console.log(marks , typeof marks);
+
+// Functions
+function findName(){
+
+}
+console.log(typeof findName);
+
+// Dates
+let date = new Date();
+console.log(typeof date); // object
